d92bad49ec2d7973d7c196b4855eeb5ccc9bceb4
Takuya Urakawa <urkwtky@gmail.com>
2018-07-29 13:53:52 +0900

    add typing animation

--- keyboards/ergoinu/keymaps/default/keymap.c
+++ keyboards/ergoinu/keymaps/default/keymap.c
@@ -7,8 +7,6 @@
 extern rgblight_config_t rgblight_config;
 #endif
 
-int RGB_current_mode;
-
 #define JA_CLON KC_QUOT  // : and +
 #define JA_AT   KC_LBRC  // @ and `
 #define JA_HAT  KC_EQL   // ^ and ~
@@ -24,7 +22,8 @@ enum LAYER_NO {
 };
 
 enum CUSTOM_KEYCODES {
-  RGB_RST = SAFE_RANGE
+  RGB_RST = SAFE_RANGE,
+  RGB_TYPE,
 };
 
 // Fillers to make layering more clear
@@ -34,7 +33,7 @@ enum CUSTOM_KEYCODES {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [BASE] = LAYOUT( \
-    KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,   KC_8,    KC_9,    KC_O,    KC_MINS, KC_LBRC,   KC_JYEN, \
+    KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,   KC_8,    KC_9,    KC_O,    KC_MINS, JA_HAT,    KC_JYEN, \
     KC_DEL,    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    JA_AT,     JA_LBRC, \
     KC_LCTRL,           KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, JA_CLON,   JA_RBRC, \
     KC_LSHIFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RO,     KC_RSFT, \
@@ -49,14 +48,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                         ______,  ______,  ______,  ______,  ______,  ______,  KC_DEL,  KC_HENK, ______,  ______ \
   ),
  [CONF] = LAYOUT( \
-    MO(CONF),  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,   XXXXXX, XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  MO(CONF), \
-    RGB_RST,   RGB_TOG, RGB_HUI, RGB_MOD, XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  RGB_RST, \
+    MO(CONF),  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,   XXXXXX, XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX, \
+    RGB_RST,   RGB_TOG, RGB_HUI, RGB_MOD, RGB_TYPE,XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX, \
     KC_LCTRL,           XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX, \
     KC_LSHIFT,          XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX, \
                         XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX \
   )
 
 };
+
+bool enableLEDTypeAnime = false;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch(keycode) {
@@ -65,26 +66,38 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         if (record->event.pressed) {
           eeconfig_update_rgblight_default();
           rgblight_enable();
-          RGB_current_mode = rgblight_config.mode;
         }
       #endif
       break;
-    case RGB_MOD:
+    // case RGB_MOD:
+    //   #ifdef RGBLIGHT_ENABLE
+    //       if (record->event.pressed) {
+    //         rgblight_mode(RGB_current_mode);
+    //         rgblight_step();
+    //       }
+    //   #endif
+    //   return false;
+    //   break;
+    case RGB_TYPE:
       #ifdef RGBLIGHT_ENABLE
           if (record->event.pressed) {
-            rgblight_mode(RGB_current_mode);
-            rgblight_step();
-            RGB_current_mode = rgblight_config.mode;
+            rgblight_enable_noeeprom();
+            rgblight_mode_noeeprom(1);
+            enableLEDTypeAnime = !enableLEDTypeAnime;
           }
       #endif
       return false;
       break;
   }
+
+  if(enableLEDTypeAnime) {
+    rgblight_mode_noeeprom(1);
+    uint16_t hue = (rgblight_config.hue + 5) % 360;
+    rgblight_sethsv_noeeprom(hue, rgblight_config.sat, rgblight_config.val);
+  }
+
   return true;
 }
 
 void matrix_init_user(void) {
-    #ifdef RGBLIGHT_ENABLE
-      RGB_current_mode = rgblight_config.mode;
-    #endif
 }
