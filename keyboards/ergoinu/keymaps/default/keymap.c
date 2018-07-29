b6e6699a9b72d859f7d818a4d15ea158cf8ee8fa
hsgw <urkwtky@gmail.com>
2018-07-29 18:02:38 +0900

    Update keymap

--- keyboards/ergoinu/keymaps/default/keymap.c
+++ keyboards/ergoinu/keymaps/default/keymap.c
@@ -33,28 +33,27 @@ enum CUSTOM_KEYCODES {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [BASE] = LAYOUT( \
-    KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,   KC_8,    KC_9,    KC_O,    KC_MINS, JA_HAT,    KC_JYEN, \
-    KC_DEL,    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    JA_AT,     JA_LBRC, \
-    KC_LCTRL,           KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, JA_CLON,   JA_RBRC, \
-    KC_LSHIFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RO,     KC_RSFT, \
-                        MO(META),KC_LALT, KC_RGUI, KC_ZKHK, KC_SPC,  KC_ENT, KC_BSPC, KC_MHEN, KC_KANA, MO(META) \
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,             KC_5,    KC_6,    KC_7,   KC_8,    KC_9,    KC_O,    KC_MINS, JA_HAT,   KC_JYEN, \
+    KC_DEL,  KC_TAB,  KC_Q,    KC_W,    KC_E,             KC_R,    KC_T,    KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    JA_AT,    JA_LBRC, \
+    KC_LCTL,          KC_A,    KC_S,    KC_D,             KC_F,    KC_G,    KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, JA_CLON,  JA_RBRC, \
+    KC_LSFT,          KC_Z,    KC_X,    KC_C,             KC_V,    KC_B,    KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RO,    KC_RSFT, \
+                      MO(META),KC_LALT, LSFT_T(KC_ZKHK),  KC_ENT,  KC_SPC,  KC_ENT, KC_BSPC, KC_MHEN, KC_KANA, MO(META) \
   ),
 
   [META] = LAYOUT( \
-    MO(CONF),    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_INS, \
-    KC_DEL,    KC_CAPS, ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______, \
-    KC_LCTRL,           ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  KC_UP,   ______, \
-    KC_LSHIFT,          ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  KC_LEFT, KC_DOWN, KC_RGHT, \
-                        ______,  ______,  ______,  ______,  ______,  ______,  KC_DEL,  KC_HENK, ______,  ______ \
+    MO(CONF),KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_INS, \
+    ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  KC_SLCK, KC_PSCR, KC_HOME, KC_END, \
+    ______,           ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  KC_PAUS, KC_PGUP, KC_UP,   KC_PGDN, \
+    ______,           ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  KC_LEFT, KC_DOWN, KC_RGHT, \
+                      ______,  ______,  KC_RGUI, ______,  ______,  ______,  KC_DEL,  KC_HENK, KC_LGUI, ______ \
   ),
  [CONF] = LAYOUT( \
-    MO(CONF),  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,   XXXXXX, XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX, \
-    RGB_RST,   RGB_TOG, RGB_HUI, RGB_MOD, RGB_TYPE,XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX, \
-    KC_LCTRL,           XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX, \
-    KC_LSHIFT,          XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX, \
-                        XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX \
+    ______,  RGB_TYPE,RGB_MOD, RGB_RMOD,RGB_HUI, RGB_HUD,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX, \
+    RGB_RST, RGB_TOG, RGB_VAI, RGB_VAD, RGB_SAI, RGB_SAD,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX, \
+    XXXXXX,           XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,   XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX, \
+    XXXXXX,           XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,   XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX, \
+                      XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,   XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX \
   )
-
 };
 
 bool enableLEDTypeAnime = false;
@@ -69,15 +68,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         }
       #endif
       break;
-    // case RGB_MOD:
-    //   #ifdef RGBLIGHT_ENABLE
-    //       if (record->event.pressed) {
-    //         rgblight_mode(RGB_current_mode);
-    //         rgblight_step();
-    //       }
-    //   #endif
-    //   return false;
-    //   break;
+    case RGB_MOD:
+      #ifdef RGBLIGHT_ENABLE
+          if (record->event.pressed) {
+            enableLEDTypeAnime = false
+            rgblight_step();
+          }
+      #endif
+      return false;
+      break;
     case RGB_TYPE:
       #ifdef RGBLIGHT_ENABLE
         if (record->event.pressed) {
@@ -89,12 +88,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       return false;
       break;
   }
-
-  if(enableLEDTypeAnime) {
-    rgblight_mode_noeeprom(1);
-    uint16_t hue = (rgblight_config.hue + 5) % 360;
-    rgblight_sethsv_noeeprom(hue, rgblight_config.sat, rgblight_config.val);
-  }
+  #ifdef RGBLIGHT_ENABLE
+    if(enableLEDTypeAnime) {
+      rgblight_mode_noeeprom(1);
+      uint16_t hue = (rgblight_config.hue + 5) % 360;
+      rgblight_sethsv_noeeprom(hue, rgblight_config.sat, rgblight_config.val);
+    }
+  #endif
 
   return true;
 }
