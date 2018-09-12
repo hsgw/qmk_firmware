c50ef6f942d2dda0ca66e61751e84458f4ebdfdf
hsgw <urkwtky@gmail.com>
2018-09-12 14:47:41 +0900

    add no boatloader setting

--- keyboards/maple_test/rules.mk
+++ keyboards/maple_test/rules.mk
@@ -7,14 +7,16 @@ SRC =	matrix.c \
 # MCU_LDSCRIPT = STM32F103x8_stm32duino_bootloader
 # BOARD = GENERIC_STM32_F103
 
-# GENERIC STM32F103C8T6 board - no bootloader (programmer over serial or SWD)
-# OPT_DEFS =
-# MCU_LDSCRIPT = STM32F103x8
-# BOARD = GENERIC_STM32_F103
-
 # MAPLE MINI
-OPT_DEFS = -DCORTEX_VTOR_INIT=0x5000
-MCU_LDSCRIPT = STM32F103xB_maplemini_bootloader
+# OPT_DEFS = -DCORTEX_VTOR_INIT=0x5000
+# MCU_LDSCRIPT = STM32F103xB_maplemini_bootloader
+# BOARD = MAPLEMINI_STM32_F103
+# # If it is recent stm32duino bootloarder, uncomment below
+# DFU_ARGS = -a1 -d 1eaf:0003
+
+# MAPLE MINI - no bootloader (programmer over serial or SWD)
+OPT_DEFS =
+MCU_LDSCRIPT = STM32F103xB
 BOARD = MAPLEMINI_STM32_F103
 
 ## chip/board settings
@@ -52,8 +54,8 @@ MOUSEKEY_ENABLE = yes	# Mouse keys
 EXTRAKEY_ENABLE = no	# Audio control and System control
 CONSOLE_ENABLE = yes	# Console for debug
 COMMAND_ENABLE = yes    # Commands for debug and configuration
-SLEEP_LED_ENABLE = yes  # Breathing sleep LED during USB suspend
-NKRO_ENABLE = no	    # USB Nkey Rollover
+SLEEP_LED_ENABLE = no  # Breathing sleep LED during USB suspend
+NKRO_ENABLE = yes	    # USB Nkey Rollover
 MIDI_ENABLE = yes            # MIDI controls
 
 CUSTOM_MATRIX = yes # Custom matrix file
