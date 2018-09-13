2dd2649a6305d4e9cf4eb8c356d2b13e66e40b51
hsgw <urkwtky@gmail.com>
2018-09-13 10:22:51 +0900

    refactoring include file

--- tmk_core/protocol/midi/qmk_midi.h
+++ tmk_core/protocol/midi/qmk_midi.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #ifdef MIDI_ENABLE
+  #include <LUFA/Drivers/USB/USB.h>
   #include "midi.h"
+
   extern MidiDevice midi_device;
   void setup_midi(void);
   void send_midi_packet(MIDI_EventPacket_t* event);
