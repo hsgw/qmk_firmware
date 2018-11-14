e069dc0738357e33b40d9449b4f0ed83f6ef6992
hsgw <urkwtky@gmail.com>
2018-11-14 22:25:31 +0900

    update keymap

--- keyboards/ergoinu/keymaps/hsgw_4/keymap.c
+++ keyboards/ergoinu/keymaps/hsgw_4/keymap.c
@@ -113,31 +113,32 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 #define ______ KC_TRNS
 #define XXXXXX KC_NO
 #define META_ZK LT(META,KC_ZKHK)
+#define META_BS LT(META, KC_BSPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [BASE] = LAYOUT( \
     XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,                   XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,   XXXXXX,  XXXXXX,  XXXXXX, \
-    KC_DEL,  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    JA_AT,    JA_LBRC, \
+    KC_ESC,  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    JA_AT,    JA_LBRC, \
     KC_LCTL,          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, JA_CLON,  JA_RBRC, \
     KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                     KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RO,    KC_RSFT, \
-                               KC_LALT, KC_LGUI, META_ZK, MO(NUM), KC_SPC,  KC_ENT, KC_BSPC, MO(META), ______,   ______ \
+                               KC_LALT, KC_LGUI, META_ZK, MO(NUM), KC_SPC,  KC_ENT, MO(META), KC_BSPC, KC_RGUI,   KC_RALT \
   ),
 
   [NUM] = LAYOUT( \
     XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,                   XXXXXX, XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,   XXXXXX, \
-    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,                     KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, JA_HAT,   KC_JYEN, \
+    KC_DEL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,                     KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, JA_HAT,   KC_JYEN, \
     KC_LCTL,          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, JA_CLON,  JA_RBRC, \
     KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                     KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RO,    KC_RSFT, \
-                               KC_LALT, KC_LGUI, META_ZK, MO(NUM), KC_SPC,  KC_ENT, KC_BSPC, MO(META), ______,   ______ \
+                               ______, ______, ______, ______, KC_SPC,  KC_ENT, ______, ______, ______,   ______ \
   ),
 
   [META] = LAYOUT( \
-    XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,                   XXXXXX, XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,   XXXXXX,
-    MO(CONF),KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_INS, \
-    KC_CAPS,          ______,  ______,  KC_INS,  KC_PSCR, ______,                    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_PSCR, KC_HOME, KC_END, \
-    ______,           ______,  ______,  ______,  ______,  TD(TD_LBRT),          TD(TD_RBRT),  ______,  ______,  ______,  KC_SLCK, KC_PGUP, KC_PGDN, \\
-                               ______,  ______,  ______,  ______,  KC_ENT,  KC_SPC,  KC_DEL,  ______,  ______,  ______ \
+    MO(CONF),  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,                    XXXXXX, XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,   XXXXXX,  \
+    KC_DEL,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_INS, \
+    ______,             ______,  ______,  KC_INS,  KC_PSCR, ______,                    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_PSCR, KC_HOME, KC_END, \
+    ______,             ______,  ______,  ______,  ______,  TD(TD_LBRT),          TD(TD_RBRT),  ______,  ______,  ______,  KC_SLCK, KC_PGUP, KC_PGDN, \
+                                 ______,  ______,  ______,  ______,  KC_ENT,  KC_SPC,  META_BS,  ______,  ______,  ______ \
   ),
 
  [CONF] = LAYOUT( \
