f5eb37e525411e5b2dca0019776ab1428a55931b
hsgw <urkwtky@gmail.com>
2019-02-17 12:58:08 +0900

    update

--- keyboards/ergoinu/keymaps/hsgw_4/keymap.c
+++ keyboards/ergoinu/keymaps/hsgw_4/keymap.c
@@ -41,7 +41,7 @@ enum TAP_DANCE_KEYCODES {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [BASE] = LAYOUT( \
-    KC_DEL,  KC_M,   KC_L,  KC_SLSH,    XXXXXX,  XXXXXX,  XXXXXX,                   XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,   XXXXXX,  XXXXXX,  XXXXXX, \
+    KC_DEL,  KC_M,   KC_L,  KC_SLSH,    XXXXXX,  XXXXXX,    KC_I,                   XXXXXX,  XXXXXX,  XXXXXX,  XXXXXX,   XXXXXX,  XXXXXX,  XXXXXX, \
     KC_ESC,  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    JA_AT,    JA_LBRC, \
     KC_LCTL,          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, JA_CLON,  JA_RBRC, \
     KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                     KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RO,    KC_RSFT, \
