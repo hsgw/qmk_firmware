8749cc44eb62ad5cfefa39aea8109e4d9bf7dd7e
hsgw <urkwtky@gmail.com>
2019-05-20 13:25:03 +0900

    change USB PID/VID, power consumption to 100mA

--- keyboards/choco/config.h
+++ keyboards/choco/config.h
@@ -21,11 +21,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x0000
+#define PRODUCT_ID      0x464D
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Dm9 Records
 #define PRODUCT         choco
-#define DESCRIPTION     A custom keyboard
+#define DESCRIPTION     7x4 ortholinear onehand keyboard with kailh choc switch
+
+#define USB_MAX_POWER_CONSUMPTION 100
 
 /* key matrix size */
 #define MATRIX_ROWS 1
