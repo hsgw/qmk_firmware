9948cdb3c94893957874f9fff3d0471bd949b85a
hsgw <urkwtky@gmail.com>
2019-10-15 13:51:16 +0900

    add game keymap

--- keyboards/lain/keymaps/hsgw/keymap.c
+++ keyboards/lain/keymaps/hsgw/keymap.c
@@ -22,6 +22,7 @@
 #define GUI_ZH  LGUI_T(JP_ZHTG)
 #define FUNC_BS LT(FUNC,KC_BSPC)
 #define TAB_CTL LCTL_T(KC_TAB)
+#define G2_GUI LT(GAME2,KC_LGUI)
 
 // Defines the keycodes used by our macros in process_record_user
 // enum custom_keycodes {
@@ -29,7 +30,7 @@
 // };
 
 enum layers {
-    BASE, NUM, FUNC, CONF
+    BASE, NUM, FUNC, CONF, GAME, GAME2
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -38,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    JP_LBRC,   KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,     JP_AT, \
         TAB_CTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    JP_RBRC,   KC_H,   KC_J,    KC_K,    KC_L,    JP_SCLN,  JP_COLN,\
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,               KC_INS, KC_N,    KC_M,    JP_COMM, JP_DOT,   JP_SLSH, \
-        KC_LCTL,                   KC_LALT, GUI_ZH,  NUM_SPC, KC_SPC,    KC_ENT, KC_BSPC, MO(FUNC),KC_MENU, JP_CIRC,  JP_BSLS\
+        KC_LALT,                   KC_LGUI, JP_ZHTG, NUM_SPC, KC_SPC,    KC_ENT, KC_BSPC, MO(FUNC),KC_MENU, JP_CIRC,  JP_BSLS\
     ),
     [NUM] = LAYOUT(
         KC_DEL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,      KC_7,    KC_8,    KC_9,    KC_0,    JP_CIRC,  JP_YEN, \
@@ -54,10 +55,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [CONF] = LAYOUT(
         RESET,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+        TG(GAME), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
         XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX \
-    )
+    ),
+    [GAME] = LAYOUT(
+        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    JP_LBRC,   KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,     TG(GAME), \
+        KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    JP_RBRC,   KC_H,   KC_J,    KC_K,    KC_L,    JP_SCLN,  JP_COLN,\
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,               KC_PSCR, KC_N,    KC_M,    JP_COMM, JP_DOT,   JP_SLSH, \
+        KC_LCTL,                   KC_LALT, G2_GUI, KC_SPC,  KC_ENT,    KC_ENT, KC_BSPC, MO(FUNC),KC_MENU, JP_CIRC,  JP_BSLS\
+    ),
+    [GAME2] = LAYOUT(
+        XXXXXXX, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+        XXXXXXX, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+        XXXXXXX,                   XXXXXXX, G2_GUI,  XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX \
+    ),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
