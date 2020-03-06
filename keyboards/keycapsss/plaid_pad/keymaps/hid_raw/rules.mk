51e18a35f57154bf751bb0474d4450e51749c039
hsgw <urkwtky@gmail.com>
2020-03-06 21:39:31 +0900

    device to host communication is working

---
+++ keyboards/keycapsss/plaid_pad/keymaps/hid_raw/rules.mk
@@ -0,0 +1,3 @@
+MOUSEKEY_ENABLE = no       # Mouse keys
+EXTRAKEY_ENABLE = no       # Audio control and System control
+RAW_ENABLE = yes           # HID RAW communication
--- tmk_core/protocol/vusb/main.c
+++ tmk_core/protocol/vusb/main.c
@@ -57,6 +57,10 @@ int main(void) {
 #ifndef NO_UART
     uart_init(UART_BAUD_RATE);
 #endif
+#if DEBUG_LEVEL > 0
+    odDebugInit();
+#endif
+
     keyboard_setup();
 
     host_set_driver(vusb_driver());
