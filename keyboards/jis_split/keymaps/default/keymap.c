caed0d70df923483324695064d4f19360ebcc369
hsgw <urkwtky@gmail.com>
2020-03-20 02:23:02 +0900

    add jis_split

---
+++ keyboards/jis_split/keymaps/default/keymap.c
@@ -0,0 +1,67 @@
+/* Copyright 2020 Takuya Urakawa(Dm9 Records)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+#include"keymap_jp.h"
+
+// Defines names for use in layer keycodes and the keymap
+enum layer_names {
+    _BASE,
+    _FN
+};
+
+// Defines the keycodes used by our macros in process_record_user
+// enum custom_keycodes {
+//     QMKBEST = SAFE_RANGE,
+//     QMKURL
+// };
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* Base */
+    [_BASE] = LAYOUT(
+        KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,        KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_PSCR, KC_SLCK,
+        JP_ZHTG, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    JP_MINS, JP_CIRC, JP_YEN, KC_BSPC, KC_INS, KC_HOME,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JP_AT,   JP_LBRC, KC_DEL, KC_END,
+        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    JP_SCLN, JP_COLN, JP_RBRC, KC_ENT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    JP_LT,   JP_GT,   JP_SLSH, JP_BSLS, KC_RSFT, KC_UP,
+        KC_LCTL, KC_LGUI, KC_LALT, JP_MHEN, MO(_FN), KC_SPC,       KC_SPC,  MO(_FN), JP_HENK, JP_KANA, KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN,KC_RGHT
+    ),
+    [_FN] = LAYOUT(
+        _______,          _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PAUS,
+        _______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGUP,
+        _______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, _______, _______, KC_PGDN,
+        _______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, _______, _______,_______
+    )
+};
+
+// bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+//     return true;
+// }
+
+/*
+void matrix_init_user(void) {
+
+}
+
+void matrix_scan_user(void) {
+
+}
+
+bool led_update_user(led_t led_state) {
+    return true;
+}
+*/
