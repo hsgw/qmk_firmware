edf104a1317249be015fe6b9becb6f1f6982704d
hsgw <urkwtky@gmail.com>
2020-03-22 03:36:55 +0900

    update keymap

--- keyboards/jis_split/keymaps/default/keymap.c
+++ keyboards/jis_split/keymaps/default/keymap.c
@@ -35,11 +35,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         JP_ZHTG, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    JP_MINS, JP_CIRC, JP_YEN, KC_BSPC, KC_INS, KC_HOME,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JP_AT,   JP_LBRC, KC_DEL, KC_END,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    JP_SCLN, JP_COLN, JP_RBRC, KC_ENT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    JP_LT,   JP_GT,   JP_SLSH, JP_BSLS, KC_RSFT, KC_UP,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    JP_COMM, JP_DOT,  JP_SLSH, JP_BSLS, KC_RSFT, KC_UP,
         KC_LCTL, KC_LGUI, KC_LALT, JP_MHEN, MO(_FN), KC_SPC,       KC_SPC,  MO(_FN), JP_HENK, JP_KANA, KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN,KC_RGHT
     ),
     [_FN] = LAYOUT(
-        _______,          _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PAUS,
+        RESET,            _______, _______, _______, _______,      RESET,   _______, _______, _______, _______, _______, _______, _______, _______, KC_PAUS,
         _______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGUP,
         _______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, _______, _______, KC_PGDN,
         _______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, _______, _______,
