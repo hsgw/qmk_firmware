db901de3c5128e17dcf514676a06eb12392cb164
hsgw <urkwtky@gmail.com>
2020-03-29 22:40:53 +0900

    add test keymaps

---
+++ keyboards/keycapsss/plaid_pad/keymaps/hid_raw/readme.md
@@ -0,0 +1,1 @@
+# raw hid test keymap
---
+++ keyboards/keycapsss/plaid_pad/keymaps/hid_raw/rules.mk
@@ -0,0 +1,5 @@
+ENCODER_ENABLE = no
+
+MOUSEKEY_ENABLE = no       # Mouse keys
+EXTRAKEY_ENABLE = no       # Audio control and System control
+RAW_ENABLE = yes           # HID RAW communication
