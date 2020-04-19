597ca256484f82a79a483ff30b5bc65ccf920580
hsgw <urkwtky@gmail.com>
2020-04-19 14:00:21 +0900

    update my keymap

--- keyboards/lain/keymaps/hsgw/keymap.c
+++ keyboards/lain/keymaps/hsgw/keymap.c
@@ -19,11 +19,12 @@
 
 #define NUM_SPC LT(NUM,KC_SPC)
 #define S_SPC   LSFT_T(KC_SPC)
-#define GUI_ZH  LGUI_T(JP_ZHTG)
+#define CTL_ZH  LCTL_T(JP_ZHTG)
 #define FUNC_BS LT(FUNC,KC_BSPC)
 #define TAB_CTL LCTL_T(KC_TAB)
 #define G2_GUI LT(GAME2,KC_LGUI)
-#define WIN_PS
+#define WIN_PS LSFT(LGUI(KC_S))
+#define VS_FMT LSFT(LCTL(KC_F))
 
 // Defines the keycodes used by our macros in process_record_user
 // enum custom_keycodes {
@@ -40,18 +41,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    JP_LBRC,   KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,     JP_AT, \
         TAB_CTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    JP_RBRC,   KC_H,   KC_J,    KC_K,    KC_L,    JP_SCLN,  JP_COLN,\
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,               KC_INS, KC_N,    KC_M,    JP_COMM, JP_DOT,   JP_SLSH, \
-        KC_LALT,                   KC_LGUI, JP_ZHTG, NUM_SPC, TT(XKEY),  KC_ENT, KC_BSPC, MO(FUNC),KC_MENU, JP_CIRC,  JP_BSLS\
+        KC_LALT,                   KC_LGUI, CTL_ZH,  NUM_SPC, TT(XKEY),  KC_ENT, KC_BSPC, MO(FUNC),KC_MENU, JP_CIRC,  JP_BSLS\
     ),
     [NUM] = LAYOUT(
-        KC_DEL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,      KC_7,    KC_8,    KC_9,    KC_0,    JP_CIRC,  JP_YEN, \
-        _______, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), S(KC_6),   S(KC_7), S(KC_8), S(KC_9), JP_MINS, S(JP_MINS),S(JP_YEN), \
-        _______, _______, _______, _______, _______, _______,            KC_PSCR, _______, JP_LBRC, JP_RBRC, _______,  _______, \
-        _______,                   _______, _______, _______, _______,   _______, _______, _______, _______, _______,  _______ \
+        KC_DEL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,      KC_7,    KC_8,    KC_9,    KC_0,      JP_CIRC,   JP_YEN, \
+        _______, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), S(KC_6),   S(KC_7), S(KC_8), S(KC_9), JP_MINS,   S(JP_MINS),S(JP_YEN), \
+        _______, _______, _______, _______, _______, _______,            WIN_PS,  JP_LBRC, JP_RBRC, S(JP_COMM),S(JP_DOT), _______, \
+        _______,                   _______, _______, _______, _______,   _______, _______, _______, _______,   _______,  _______ \
     ),
     [FUNC] = LAYOUT(
         _______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
-        _______, _______, _______, _______, _______, _______, _______,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, \
-        _______, _______, _______, _______, _______, _______,             KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______, \
+        _______, _______, _______, _______, _______, _______, _______,    _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, \
+        _______, _______, _______, _______, _______, _______,             _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, \
         _______,                   _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______ \
     ),
     [CONF] = LAYOUT(
