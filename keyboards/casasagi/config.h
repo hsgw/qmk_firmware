e1a35cb2971bbd18ce57f77f2c35586a31d2a856
Takuya Urakawa <urkwkty@gmail.com>
2021-04-15 17:16:31 +0900

    add casasagi

---
+++ keyboards/casasagi/config.h
@@ -0,0 +1,147 @@
+/*
+Copyright 2021 SMKiJ Casasagi Contributors
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID 0xFEED
+#define PRODUCT_ID 0x0000
+#define DEVICE_VER 0x0001
+#define MANUFACTURER SMKiJ Casasagi Contributors
+#define PRODUCT casasagi
+
+/* key matrix size */
+#define MATRIX_ROWS 8
+#define MATRIX_COLS 7
+
+/*
+ * Keyboard Matrix Assignments
+ *
+ * Change this to how you wired your keyboard
+ * COLS: AVR pins used for columns, left to right
+ * ROWS: AVR pins used for rows, top to bottom
+ * DIODE_DIRECTION: COL2ROW = COL = Anode (+), ROW = Cathode (-, marked on diode)
+ *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
+ *
+ */
+
+#define MATRIX_ROW_PINS \
+    { A15, A7, A5, A6 }
+
+#define MATRIX_ROW_PINS_RIGHT \
+    { A15, A4, A3, A2 }
+
+#define MATRIX_COL_PINS \
+    { B2, B1, B0, B8, B5, B4, B6 }
+
+#define MATRIX_COL_PINS_RIGHT \
+    { B2, B1, B0, A7, C13, B9, B8 }
+
+#define UNUSED_PINS
+
+/* COL2ROW, ROW2COL */
+#define DIODE_DIRECTION COL2ROW
+
+/* RGBLED setting */
+#define RGBLIGHT_ANIMATIONS
+#define RGBLIGHT_SPLIT \
+    { 28, 28 }
+
+#define WS2812_SPI SPID2
+#define RGB_DI_PIN B15
+#define RGBLED_NUM 56
+#define WS2812_SPI_MOSI_PAL_MODE 0
+#define WS2812_EXTERNAL_PULLUP
+
+/* Split communication setting */
+#define SOFT_SERIAL_PIN B6  // USART TX pin
+#define SELECT_SOFT_SERIAL_SPEED \
+    1                               // or 0, 2, 3, 4, 5
+                                    //  0: about 460800 baud
+                                    //  1: about 230400 baud (default)
+                                    //  2: about 115200 baud
+                                    //  3: about 57600 baud
+                                    //  4: about 38400 baud
+                                    //  5: about 19200 baud
+#define SERIAL_USART_DRIVER SD1     // USART driver of TX pin. default: SD1
+#define SERIAL_USART_TX_PAL_MODE 0  // Pin "alternate function", see the respective datasheet for the appropriate values for your MCU. default: 7
+
+#define SPLIT_USB_DETECT
+#define SPLIT_HAND_PIN B12
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCE 5
+
+/* define if matrix has ghost (lacks anti-ghosting diodes) */
+//#define MATRIX_HAS_GHOST
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+/* If defined, GRAVE_ESC will always act as ESC when CTRL is held.
+ * This is useful for the Windows task manager shortcut (ctrl+shift+esc).
+ */
+//#define GRAVE_ESC_CTRL_OVERRIDE
+
+/*
+ * Force NKRO
+ *
+ * Force NKRO (nKey Rollover) to be enabled by default, regardless of the saved
+ * state in the bootmagic EEPROM settings. (Note that NKRO must be enabled in the
+ * makefile for this to work.)
+ *
+ * If forced on, NKRO can be disabled via magic key (default = LShift+RShift+N)
+ * until the next keyboard reset.
+ *
+ * NKRO may prevent your keystrokes from being detected in the BIOS, but it is
+ * fully operational during normal computer usage.
+ *
+ * For a less heavy-handed approach, enable NKRO via magic key (LShift+RShift+N)
+ * or via bootmagic (hold SPACE+N while plugging in the keyboard). Once set by
+ * bootmagic, NKRO mode will always be enabled until it is toggled again during a
+ * power-up.
+ *
+ */
+//#define FORCE_NKRO
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+
+/* disable these deprecated features by default */
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
+
+/* Bootmagic Lite key configuration */
+//#define BOOTMAGIC_LITE_ROW 0
+//#define BOOTMAGIC_LITE_COLUMN 0
