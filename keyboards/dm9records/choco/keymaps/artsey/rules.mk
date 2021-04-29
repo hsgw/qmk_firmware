15944f8e5ab2ee9a2162af8a86959362b07ca2b2
hsgw <urkwtky@gmail.com>
2021-04-29 16:52:20 +0900

    add artsey

---
+++ keyboards/dm9records/choco/keymaps/artsey/rules.mk
@@ -0,0 +1,15 @@
+# project specific files
+SRC = artsey.c macros.c
+
+# Build Options
+#   comment out to disable the options.
+#
+
+#BOOTMAGIC_ENABLE = yes	# Virtual DIP switch configuration
+MOUSEKEY_ENABLE = yes	# Mouse keys
+EXTRAKEY_ENABLE = yes	# Audio control and System control
+CONSOLE_ENABLE = no	# Console for debug
+COMMAND_ENABLE = no    # Commands for debug and configuration
+SLEEP_LED_ENABLE = no  # Breathing sleep LED during USB suspend
+NKRO_ENABLE = yes	    # USB Nkey Rollover
+CUSTOM_MATRIX = lite
