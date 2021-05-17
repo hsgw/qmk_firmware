f4c6ffd49d1268c680cd304d4b8d41459a65b2b9
hsgw <urkwtky@gmail.com>
2021-05-18 02:25:42 +0900

    add rgb matrix support

---
+++ keyboards/casasagi/keymaps/eeprom/config.h
@@ -0,0 +1,1 @@
+#define SPLIT_TRANSPORT_MIRROR
---
+++ keyboards/casasagi/keymaps/rgbmatrix/rules.mk
@@ -0,0 +1,5 @@
+CONSOLE_ENABLE = yes        # Console for debug
+COMMAND_ENABLE = yes        # Commands for debug and configuration
+
+RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
+RGB_MATRIX_ENABLE = yes     # Enable keyboard RGB matrix (do not use together with RGBLIGHT_ENABLE)
