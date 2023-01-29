230aeb47dc304c6589aa555ae46d87a3a5cf1133
hsgw <urkwtky@gmail.com>
2023-01-29 23:43:12 +0900

    update

---
+++ keyboards/dm9records/choco_new/rules.mk
@@ -0,0 +1,6 @@
+# linker script to use
+# it should exist either in <chibios>/os/common/ports/ARMCMx/compilers/GCC/ld/
+#  or <this_dir>/ld/
+MCU_LDSCRIPT = STM32F072x8
+
+# DFU_ARGS = -d 0483:df11 -a 0 -s 0x08000000:leave
