27d9a9747f8657addc1022e0d70ad487705c521b
hsgw <urkwtky@gmail.com>
2024-02-04 18:02:11 +0900

    add frannel

---
+++ keyboards/dm9records/frannel/config.h
@@ -0,0 +1,23 @@
+// Copyright 2024 Takuya Urakawa (@hsgw)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define LED_0_PIN "GP28"
+#define LED_1_PIN "GP29"
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
---
+++ keyboards/dm9records/frannel/keymaps/default/keymap.c
@@ -0,0 +1,16 @@
+// Copyright 2024 Takuya Urakawa
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+// clang-format off
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT_ortholinear(
+        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,    KC_K,    KC_L,
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,    KC_K,    KC_L,
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,    KC_K,    KC_L,
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,    KC_K,    KC_L
+    )
+};
+// clang-format on
