0b8ec5a63bfe2efbe959d3dc1c47d42dce51a7e8
hsgw <urkwtky@gmail.com>
2024-02-25 02:28:20 +0900

    update default keymap

--- keyboards/dm9records/ichi_pp/keymaps/default/keymap.c
+++ keyboards/dm9records/ichi_pp/keymaps/default/keymap.c
@@ -6,9 +6,9 @@
 #define DESKLIGHT_STEP_MAX 3
 #define DESKLIGHT_BRIGHTNESS RGBLIGHT_LIMIT_VAL / DESKLIGHT_STEP_MAX
 
-uint16_t time_bigsw       = 0;
-bool     on_alt_layer[2]  = {false, false};
-uint16_t time_sw[2]       = {0, 0};
+uint16_t bigsw_timer      = 0;
+bool     sw_holding[2]    = {false, false};
+uint16_t sw_timer[2]      = {0, 0};
 bool     enable_desklight = true;
 uint8_t  desklight_step   = DESKLIGHT_STEP_MAX;
 
@@ -46,8 +46,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case BIGSW:
             if (record->event.pressed) {
-                time_bigsw = record->event.time;
-                if (on_alt_layer[0] || on_alt_layer[1]) {
+                bigsw_timer = record->event.time;
+                if (sw_holding[0] || sw_holding[1]) {
                     set_desklight_mode(!enable_desklight);
                 } else {
                     if (enable_desklight) {
@@ -62,7 +62,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                     }
                 }
             } else {
-                if (TIMER_DIFF_16(record->event.time, time_bigsw) > BIG_SWITCH_HOLD_TIME) {
+                if (TIMER_DIFF_16(record->event.time, bigsw_timer) > BIG_SWITCH_HOLD_TIME) {
                     // save config to eeprom
                     rgblight_mode(rgblight_get_mode());
                     rgblight_sethsv(rgblight_get_hue(), rgblight_get_sat(), rgblight_get_val());
@@ -71,12 +71,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             return false;
         case SW1:
             if (record->event.pressed) {
-                time_sw[0]      = record->event.time;
-                on_alt_layer[0] = true;
+                sw_timer[0]   = record->event.time;
+                sw_holding[0] = true;
             } else {
-                on_alt_layer[0] = false;
-                if (TIMER_DIFF_16(record->event.time, time_sw[0]) < TAPPING_TERM) {
-                    if (on_alt_layer[1]) {
+                sw_holding[0] = false;
+                if (TIMER_DIFF_16(record->event.time, sw_timer[0]) < TAPPING_TERM) {
+                    if (sw_holding[1]) {
                         if (rgblight_get_sat() == 255) {
                             rgblight_sethsv_noeeprom(rgblight_get_hue(), 0, rgblight_get_val());
                         } else {
@@ -90,12 +90,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             return false;
         case SW2:
             if (record->event.pressed) {
-                time_sw[1]      = record->event.time;
-                on_alt_layer[1] = true;
+                sw_timer[1]   = record->event.time;
+                sw_holding[1] = true;
             } else {
-                on_alt_layer[1] = false;
-                if (TIMER_DIFF_16(record->event.time, time_sw[1]) < TAPPING_TERM) {
-                    if (on_alt_layer[0]) {
+                sw_holding[1] = false;
+                if (TIMER_DIFF_16(record->event.time, sw_timer[1]) < TAPPING_TERM) {
+                    if (sw_holding[0]) {
                         if (rgblight_get_val() >= RGBLIGHT_LIMIT_VAL) {
                             rgblight_sethsv_noeeprom(rgblight_get_hue(), rgblight_get_sat(), 0);
                         } else {
