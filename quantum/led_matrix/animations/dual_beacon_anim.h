505757fe0b91f29dfb6b83f0c431b700cfbc4948
hsgw <urkwtky@gmail.com>
2026-03-09 14:43:41 +0900

    led_matrix: Support 256 or more LEDs by extending index to uint16_t

--- quantum/led_matrix/animations/dual_beacon_anim.h
+++ quantum/led_matrix/animations/dual_beacon_anim.h
@@ -2,7 +2,7 @@
 LED_MATRIX_EFFECT(DUAL_BEACON)
 #    ifdef LED_MATRIX_CUSTOM_EFFECT_IMPLS
 
-static uint8_t DUAL_BEACON_math(uint8_t val, int8_t sin, int8_t cos, uint8_t i, uint8_t time) {
+static uint8_t DUAL_BEACON_math(uint8_t val, int8_t sin, int8_t cos, led_index_t i, uint8_t time) {
     return scale8(((g_led_config.point[i].y - k_led_matrix_center.y) * cos + (g_led_config.point[i].x - k_led_matrix_center.x) * sin) / 128, val);
 }
 
--- quantum/led_matrix/animations/runners/effect_runner_sin_cos_i.h
+++ quantum/led_matrix/animations/runners/effect_runner_sin_cos_i.h
@@ -1,6 +1,6 @@
 #pragma once
 
-typedef uint8_t (*sin_cos_i_f)(uint8_t val, int8_t sin, int8_t cos, uint8_t i, uint8_t time);
+typedef uint8_t (*sin_cos_i_f)(uint8_t val, int8_t sin, int8_t cos, led_index_t i, uint8_t time);
 
 bool effect_runner_sin_cos_i(effect_params_t* params, sin_cos_i_f effect_func) {
     LED_MATRIX_USE_LIMITS(led_min, led_max);
@@ -8,7 +8,7 @@ bool effect_runner_sin_cos_i(effect_params_t* params, sin_cos_i_f effect_func) {
     uint16_t time      = scale16by8(g_led_timer, led_matrix_eeconfig.speed / 4);
     int8_t   cos_value = cos8(time) - 128;
     int8_t   sin_value = sin8(time) - 128;
-    for (uint8_t i = led_min; i < led_max; i++) {
+    for (led_index_t i = led_min; i < led_max; i++) {
         LED_MATRIX_TEST_LED_FLAGS();
         led_matrix_set_value(i, effect_func(led_matrix_eeconfig.val, cos_value, sin_value, i, time));
     }
